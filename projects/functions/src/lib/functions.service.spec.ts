dfc8ff0b9fe463a79445ec64727948ea464d9788
maksymzav <maksym.zavorotynskyi@shopping-feed.com>
2021-12-03 12:52:38 +0200

    Add reverseString() and reverseNumber() functions.

--- projects/functions/src/lib/functions.service.spec.ts
+++ projects/functions/src/lib/functions.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { FunctionsService } from './functions.service';
+import {FunctionsService} from './functions.service';
 
 describe('FunctionsService', () => {
   let service: FunctionsService;
@@ -12,5 +12,29 @@ describe('FunctionsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('service.reverseString()', () => {
+    it('should reverse a string 12345', () => {
+      expect(service.reverseString('12345')).toBe('54321');
+    });
+
+    it('should reverse a string laptop', () => {
+      expect(service.reverseString('laptop')).toBe('potpal');
+    });
+
+    it('should reverse a string with uppercase letters', () => {
+      expect(service.reverseString('tAblE')).toBe('ElbAt');
+    });
+  });
+
+  describe('service.reverseNumber()', () => {
+    it('should reverse a number', () => {
+      expect(service.reverseNumber(12345)).toBe(54321);
+    });
+
+    it('should reverse a number with a 0', () => {
+      expect(service.reverseNumber(123450)).toBe(54321);
+    });
   });
 });
--- projects/functions/src/lib/functions.service.ts
+++ projects/functions/src/lib/functions.service.ts
@@ -6,4 +6,12 @@ import { Injectable } from '@angular/core';
 export class FunctionsService {
 
   constructor() { }
+
+  reverseString(input: string): string {
+    return '';
+  }
+
+  reverseNumber(input: number): number {
+    return 0;
+  }
 }
